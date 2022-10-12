98f3d2e4dec3c41d05be5dd7061e804dce6a6afd
sagoresarker <sagoresarker.cse@gmail.com>
2022-10-13 04:19:11 +0600

    Add commenting features

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
